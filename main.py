f9e89246a30e8905bfb08fbdbb5b79960d24251a
petit <petitoff@outlook.com>
2021-10-23 19:24:33 +0200

    Adding a new thread that starts handling of telegram messages

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from core import *
 
+# telegram bot and sending message (all api and other main def)
+thread1 = Thread(target=telegram_main)
+
+thread1.setDaemon(True)
+thread1.start()
 try:
     while True:
         pass
