178126362846dc7d98a3ad1022a70e6f70963659
petit <petitoff@outlook.com>
2021-10-23 20:24:20 +0200

    Adding options and commands in the Telegram section. Adding test functions.

--- core.py
+++ core.py
@@ -5,6 +5,8 @@ from telegram.ext import *
 import json
 from threading import Thread
 
+# Global variables
+
 # Import keys to api coinbase pro and telegram
 with open("keys.json", 'r') as f:
     api_keys = json.loads(f.read())
@@ -19,11 +21,24 @@ auth_client = cbpro.AuthenticatedClient(coinbase_pro_public_key, coinbase_pro_se
 
 public_client = cbpro.PublicClient()
 
-"""Auxiliary functions"""
+""" Auxiliary functions """
 # Downloading all information about cryptocurrencies.
 result_about_all_cryptocurrencies = public_client.get_products()
+print(result_about_all_cryptocurrencies[0])
+print(public_client.get_product_ticker("BTC-EUR"))
+print(public_client.get_product_24hr_stats("BTC-EUR"))
+# for result in result_about_all_cryptocurrencies:
+#     print(result["id"])
 
-"""Telegram"""
+""" Main Function """
+
+
+# Live cryptocurrency price
+def live_price_cryptocurrency():
+    pass
+
+
+""" Telegram """
 
 
 def start_command(update, context):
@@ -42,12 +57,16 @@ def status_command(update, context):
     update.message.reply_text("I'm alive and well.")
 
 
-def change_settings(update, context):
+def settings_and_functions(update, context):
     json_id = update
 
     if json_id["message"]["chat"]["id"] != 1181399908:
         update.message.reply_text("You don't have permission.")
         return
+
+    text = str(update.message.text).lower()
+    if text[:5] == "price":
+        pass
 
 
 bot_settings = Bot(telegram_settings_api_main)
@@ -66,6 +85,6 @@ def telegram_main():
     dp.add_handler(CommandHandler("help", help_command))
     dp.add_handler(CommandHandler("status", status_command))
 
-    dp.add_handler(MessageHandler(Filters.text, change_settings))
+    dp.add_handler(MessageHandler(Filters.text, settings_and_functions))
 
     updater.start_polling(1)
