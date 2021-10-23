ff46252e94ab4035ddbdc698aa080bec20565725
petit <petitoff@outlook.com>
2021-10-23 22:13:09 +0200

    Addition of starting auxiliary function.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from core import *
 
+# Starting auxiliary functions
+get_list_of_all_crypto_to_euro()
+
 # telegram bot and sending message (all api and other main def)
 thread1 = Thread(target=telegram_main)
 
