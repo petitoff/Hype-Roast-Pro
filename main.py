77bd71730ae267f131e99a09529ca15bbcbb9cc2
petit <petitoff@outlook.com>
2021-10-24 12:07:07 +0200

    Live price function launch added.

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ get_list_of_all_crypto_to_euro()
 
 # telegram bot and sending message (all api and other main def)
 thread1 = Thread(target=telegram_main)
+thread2 = Thread(target=live_price_cryptocurrency)
 
 thread1.setDaemon(True)
 thread1.start()
+
+thread2.setDaemon(True)
+thread2.start()
+
 try:
     while True:
         pass
